eea48e46778a34faf452aa6b42ff606c2932f90b
thien0709 <thien070904@gmail.com>
2023-04-18 20:09:54 +0700

    update

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function User(firstName, lastName, age, comment){
 }
 let  author = new User("Bach", "Thien", 18, "Toi la mot nguoi code do");
 console.log(author);
+let User = function(firstName, lastName, age, comment){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.comment = comment;
+}
+let user = new User("Bach Xuan", "Thien", 18, "Toi la mot nguoi rat vui");
+console.log(user);
