500e44ce491e5d707bd2d33745eb346357b8809c
thien0709 <thien070904@gmail.com>
2023-04-26 22:09:10 +0700

    commit condition

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-function User(firstName, lastName, age, comment){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.comment = comment;
-}
-let  author = new User("Bach", "Thien", 18, "Toi la mot nguoi code do");
-console.log(author);
-let User = function(firstName, lastName, age, comment){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.comment = comment;
-}
-let user = new User("Bach Xuan", "Thien", 18, "Toi la mot nguoi rat vui");
-console.log(user);
+// // Degradation function
+// function User(firstName, lastName, age, comment){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.comment = comment;
+// }
+// let  author = new User("Bach", "Thien", 18, "Toi la mot nguoi code do");
+// console.log(author);
+// Expression function
+// let User = function(firstName, lastName, age, comment){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.comment = comment;
+// }
+
+// let user = new User("Bach Xuan", "Thien", 18, "Toi la mot nguoi rat vui");
+// console.log(user);
+
+// For/of
+// let myClass = [
+//     'Name1',
+//     'Name2',
+// ]
+// for(let value of myClass){
+//     console.log(value);
+// }
+
+//
