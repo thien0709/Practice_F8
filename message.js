53c360c251c2155a3d60fc5eb5879ae5343ce314
thien0709 <thien070904@gmail.com>
2024-04-09 15:48:57 +0700

    update

--- message.js
+++ message.js
@@ -2,27 +2,22 @@ let messageUser = document.querySelector(".listMessage");
 let listUser;
 function getMessage() {
   listUser = document.querySelectorAll(".list h4");
+  let nameUser = document.querySelector("#message .name");
   listUser.forEach((arg) => {
     arg.addEventListener("click", () => {
+      nameUser.innerHTML = `${arg.className}`;
       let personObject = data.filter(
         (personObj) => personObj.person === arg.className
       );
       personObject.forEach((personObj) => {
-        console.log(personObj.messages);
         setMessage(personObj.messages);
       });
     });
   });
 };
-
-const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      if (mutation.addedNodes.length || mutation.removedNodes.length) {
-        getMessage();
-      }
-    });
-  });
-observer.observe(document.querySelector('.list'), { childList: true, subtree: true });
+setInterval(() => {
+ getMessage();
+}, 100); 
 // Add message to listMessage
 let setMessage = function (arr) {
   let htmls = "";
