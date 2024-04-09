cda0364653fcc11d660c1d845b4b94bf751a1739
thien0709 <thien070904@gmail.com>
2024-04-09 10:23:48 +0700

    update

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-let text = "Thien123\b"
-let text1 = "thien123\f"
-var myString = "Hello\bWorld";
-console.log(myString);
-// Everything without a "Value" is false
-let x = 0 // Boolean(x) == false
-let y = "" // Boolean(y) == false
-let z // Boolean(z) == false
-let o = null // Boolean(o) == false
-let i = 10 / "Hallo" // Boolean(x) == false
-// false == new Boolean(false) sử dụng dấu == thì bằng 
-// false === new Boolean(false) sử dụng dấu === thì không bằng 
-// So sánh 2 đối tượng trong JavaScript luôn luôn trả về false
-// x !== 5 false 
-// x !== "5" true
-// x !== 8 true
-// x != 8 true
-// > false
-// < true
-// >= false
-// <= true 
-// && true example: (x < 10 && y > 1) 
-// || false example:(x == 5 || y == 5)
-// ! true example: !(x == y)
-// Toán tử ?? trả về first argument nếu cả 2 không có cái nào null hoặc undefined nếu null thì trả về cái còn lại
-// Toán tử ?. trả về undefined nếu object là undefined or null 
-const car = {type:"Fiat", model:"500", color:"white"};
-let name = car?.model; // return 500
-let name1 = car?.name ; // return undefined 
+// // Sử dụng case để 2 case dùng chung một mã code
+// // case 4 : case 5 : statement break;
+
+// console.log(typeof NaN);
+// console.log(1/-0)
+// console.log(Math.floor("a"))
+
+const list = document.querySelector(".list");
+const inp = document.querySelector("#input");
+
+let filter = [];
+
+let data;
+async function getData() {
+  try {
+    const reponse = await fetch("data.json");
+    return await reponse.json();
+  } catch (error) {
+    alert(error);
+  }
+}
+// Wait get Data
+(async ()=>{
+ data = await getData();
+ setList(data);
+})();
+
+const setList =  function (arr) {
+  let htmls = "";
+  arr.forEach((arg) => {
+    const html = `
+        <li class="item">
+        <p class="${arg.person}">${arg.person}</p>
+      </li>
+        `;
+    htmls += html;
+  });
+  list.innerHTML = htmls;
+};
+inp.oninput = function (e) {
+  const value = e.target.value;
+  if (value == "") return setList(data);
+  filter = data.filter(function (item) {
+    return item.person.indexOf(value) == -1 ? false : true;
+  });
+  setList(filter);
+};
